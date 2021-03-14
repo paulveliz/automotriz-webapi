07495ea04ffa1ab4e2883daef68ce05c2da8b888
Paul Veliz <paulsystem32@gmail.com>
2021-03-14 11:20:46 -0700

    AutosController Actions
    Se imeplentaron algunas acciones en el autos controller.

--- Controllers/AutosController.cs
+++ Controllers/AutosController.cs
@@ -1,15 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using automotriz_webapi.Models;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 
 namespace automotriz_webapi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class AutosController : ControllerBase
     {
         private readonly automotrizContext Db;
@@ -20,10 +23,42 @@ namespace automotriz_webapi.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Auto>>> AutosExistentes()
+        public async Task<ActionResult<IEnumerable<Auto>>> Existentes()
         {
-            var autos = await this.Db.Autos.Take(1000).ToListAsync();
+            var autos = await this.Db.Autos
+                                    .Take(1000)
+                                    .OrderByDescending(d => d.Id)
+                                    .ToListAsync();
             return Ok(autos);
         }
+
+        [HttpGet]
+        [Route("/auto/{id}")]
+        public async Task<ActionResult<Auto>> PorId(int id)
+        {
+            var auto = await this.Db.Autos.FirstOrDefaultAsync(a => a.Id == id);
+            if(auto.Equals(null)){
+                return NotFound();
+            }else{
+                return auto;
+            }
+        }
+
+        [HttpGet]
+        [Route("/plan/{planId}")]
+        public async Task<ActionResult<IEnumerable<Auto>>> PorPlanId(int planId)
+        {
+            var autos = await this.Db.Autos
+                                  .Include(a => a.PlanFinanciamientoNavigation)
+                                  .Where(a => a.PlanFinanciamientoNavigation.Id == planId)
+                                  .ToListAsync();
+            if(autos != null && autos.Count >0){
+                return Ok(autos);
+            }else{
+                return NotFound();
+            }
+        }
+
+
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -29,10 +29,6 @@ namespace automotriz_webapi
         {
             services.AddDbContext<automotrizContext>();
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "automotriz_webapi", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,8 +37,6 @@ namespace automotriz_webapi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "automotriz_webapi v1"));
             }
 
             app.UseHttpsRedirection();
