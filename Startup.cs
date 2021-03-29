55948f5062dc358d153148b885ea3417c9ee6096
PAUL VELIZ <paulsystem32@gmail.com>
2021-03-29 12:12:39 -0700

    origins test prod

--- Startup.cs
+++ Startup.cs
@@ -31,7 +31,7 @@ namespace automotriz_webapi
         {
             services.AddCors(options => {
                 options.AddDefaultPolicy(builder => {
-                    builder.WithOrigins("https://delightful-forest-0211cd210.azurestaticapps.net/")
+                    builder.WithOrigins("*")
                             .AllowAnyHeader()
                             .AllowAnyMethod()
                             .AllowAnyOrigin();
